610b89b4edb54d795ff218f952143d528e43b452
Maximillian Arruda <dearrudam@gmail.com>
2023-08-21 02:01:10 -0300

    chore: WIP using primefaces
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ jnopo-talk/src/main/java/br/org/soujava/coffewithjava/jnopo/ChartJsView.java
@@ -0,0 +1,188 @@
+package br.org.soujava.coffewithjava.jnopo;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import org.primefaces.model.charts.ChartData;
+import org.primefaces.model.charts.axes.cartesian.CartesianScales;
+import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearAxes;
+import org.primefaces.model.charts.axes.cartesian.linear.CartesianLinearTicks;
+import org.primefaces.model.charts.bar.BarChartDataSet;
+import org.primefaces.model.charts.bar.BarChartModel;
+import org.primefaces.model.charts.bar.BarChartOptions;
+import org.primefaces.model.charts.optionconfig.animation.Animation;
+import org.primefaces.model.charts.optionconfig.legend.Legend;
+import org.primefaces.model.charts.optionconfig.legend.LegendLabel;
+import org.primefaces.model.charts.optionconfig.title.Title;
+import org.primefaces.model.charts.pie.PieChartDataSet;
+import org.primefaces.model.charts.pie.PieChartModel;
+import org.primefaces.model.charts.pie.PieChartOptions;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Named
+@ViewScoped
+public class ChartJsView implements Serializable {
+
+    private BarChartModel barModel;
+    private PieChartModel pieModel;
+
+    public BarChartModel getBarModel() {
+        return barModel;
+    }
+
+    public void refresh(){
+        createBarModel();
+        createPieModel();
+    }
+
+    @PostConstruct
+    public void init() {
+        createBarModel();
+        createPieModel();
+    }
+
+    private void createPieModel() {
+        pieModel = new PieChartModel();
+        ChartData data = new ChartData();
+
+        PieChartDataSet dataSet = new PieChartDataSet();
+
+        List<Number> values = new ArrayList<>();
+        values.add(getRandomNumberUsingInts());
+        values.add(getRandomNumberUsingInts());
+        values.add(getRandomNumberUsingInts());
+        values.add(getRandomNumberUsingInts());
+        dataSet.setData(values);
+
+        List<String> bgColors = new ArrayList<>();
+        bgColors.add("rgba(255, 99, 132, 0.5)");
+        bgColors.add("rgba(54, 162, 235, 0.5)");
+        bgColors.add("rgba(255, 205, 86, 0.5)");
+        dataSet.setBackgroundColor(bgColors);
+        dataSet.setBorderColor(bgColors);
+
+        data.addChartDataSet(dataSet);
+        List<String> labels = new ArrayList<>();
+        labels.add("Red");
+        labels.add("Blue");
+        labels.add("Yellow");
+        labels.add("Other");
+        data.setLabels(labels);
+
+
+        PieChartOptions options = new PieChartOptions();
+
+
+        // disable animation
+        Animation animation = new Animation();
+        animation.setLoop(true);
+        animation.setDuration(2);
+
+        options.setAnimation(animation);
+        pieModel.setOptions(options);
+
+        pieModel.setData(data);
+    }
+
+    public int getRandomNumberUsingInts() {
+        Random random = new Random();
+        return random.ints(0, 100)
+                .findFirst()
+                .getAsInt();
+    }
+
+    public PieChartModel getPieModel() {
+        return pieModel;
+    }
+
+    public void createBarModel() {
+        barModel = new BarChartModel();
+        ChartData data = new ChartData();
+
+        BarChartDataSet barDataSet = new BarChartDataSet();
+        barDataSet.setLabel("My First Dataset");
+
+        List<Number> values = new ArrayList<>();
+        values.add(65);
+        values.add(59);
+        values.add(80);
+        values.add(81);
+        values.add(56);
+        values.add(55);
+        values.add(40);
+        barDataSet.setData(values);
+
+        List<String> bgColor = new ArrayList<>();
+        bgColor.add("rgba(255, 99, 132, 0.2)");
+        bgColor.add("rgba(255, 159, 64, 0.2)");
+        bgColor.add("rgba(255, 205, 86, 0.2)");
+        bgColor.add("rgba(75, 192, 192, 0.2)");
+        bgColor.add("rgba(54, 162, 235, 0.2)");
+        bgColor.add("rgba(153, 102, 255, 0.2)");
+        bgColor.add("rgba(201, 203, 207, 0.2)");
+        barDataSet.setBackgroundColor(bgColor);
+
+        List<String> borderColor = new ArrayList<>();
+        borderColor.add("rgb(255, 99, 132)");
+        borderColor.add("rgb(255, 159, 64)");
+        borderColor.add("rgb(255, 205, 86)");
+        borderColor.add("rgb(75, 192, 192)");
+        borderColor.add("rgb(54, 162, 235)");
+        borderColor.add("rgb(153, 102, 255)");
+        borderColor.add("rgb(201, 203, 207)");
+        barDataSet.setBorderColor(borderColor);
+        barDataSet.setBorderWidth(1);
+
+        data.addChartDataSet(barDataSet);
+
+        List<String> labels = new ArrayList<>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+        labels.add("July");
+        data.setLabels(labels);
+        barModel.setData(data);
+
+        //Options
+        BarChartOptions options = new BarChartOptions();
+//        options.setMaintainAspectRatio(false);
+        CartesianScales cScales = new CartesianScales();
+        CartesianLinearAxes linearAxes = new CartesianLinearAxes();
+        linearAxes.setOffset(true);
+        linearAxes.setBeginAtZero(true);
+        CartesianLinearTicks ticks = new CartesianLinearTicks();
+        linearAxes.setTicks(ticks);
+        cScales.addYAxesData(linearAxes);
+        options.setScales(cScales);
+
+        Title title = new Title();
+        title.setDisplay(true);
+        title.setText("Bar Chart");
+        options.setTitle(title);
+
+        Legend legend = new Legend();
+        legend.setDisplay(true);
+        legend.setPosition("top");
+        LegendLabel legendLabels = new LegendLabel();
+        legendLabels.setFontStyle("italic");
+        legendLabels.setFontColor("#2980B9");
+        legendLabels.setFontSize(24);
+        legend.setLabels(legendLabels);
+        options.setLegend(legend);
+
+        // disable animation
+        Animation animation = new Animation();
+        animation.setDuration(0);
+        options.setAnimation(animation);
+
+        barModel.setOptions(options);
+    }
+}
